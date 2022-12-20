5a42a80fbcdd94e1429dfd2d24db1381d5fcfc12
ChrisChrome <christophercookman@gmail.com>
2022-12-20 02:30:04 -0700

    Here's a thing

---
+++ Factory/MyPlayerFactory.cs
@@ -0,0 +1,16 @@
+﻿namespace TemplatePlugin.Factory
+{
+	using System;
+	using PluginAPI.Core.Factories;
+	using PluginAPI.Core.Interfaces;
+
+	/// <summary>
+	/// A factory for <see cref="MyPlayer"/>s.
+	/// </summary>
+	public class MyPlayerFactory : PlayerFactory
+    {
+        public override Type BaseType { get; } = typeof(MyPlayer);
+
+		public override IPlayer Create(IGameComponent component) => new MyPlayer(component);
+	}
+}
